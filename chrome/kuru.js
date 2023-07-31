04fd125970c71201d5e3362794d52327f94c8b5a
Amashi <81893450+AmashiM@users.noreply.github.com>
2023-07-31 16:27:35 -0700

    Add files via upload

---
+++ chrome/kuru.js
@@ -0,0 +1,37 @@
+console.log("kuru");
+
+let kuru_image = new Image();
+
+const ThumbnailSearchQuery = "ytd-thumbnail:not(.ytd-video-preview, .ytd-rich-grid-slim-media) a > yt-image > img.yt-core-image:only-child:not(.yt-core-attributed-string__image-element),.ytp-videowall-still-image:not([style*='extension:'])";
+const SourceUrl = "https://herta.eu.org/img/hertaa1.webp";
+
+function applyThumbnail(thumbnailElement){
+    if(thumbnailElement.hasAttribute("data-kuru-loaded")){
+        return;
+    }
+    if (thumbnailElement.nodeName == "IMG") {
+        const overlayImage = document.createElement("img");
+        overlayImage.src = SourceUrl;
+        overlayImage.style.position = "absolute";
+        overlayImage.style.bottom = "0";
+        overlayImage.style.left = "0";
+        overlayImage.style.width = "50%";
+        overlayImage.style.height = "auto";
+        overlayImage.style.zIndex = "0";
+        thumbnailElement.style.position = "relative";
+        thumbnailElement.parentElement.appendChild(overlayImage);
+    } else if (thumbnailElement.nodeName == "DIV") {
+        thumbnailElement.style.backgroundImage = `url("${overlayImageURL}"), ` + thumbnailElement.style.backgroundImage;
+    }
+    thumbnailElement.setAttribute("data-kuru-loaded", "");
+}
+
+    
+function applyThumbnailToImages(){
+    document.querySelectorAll(ThumbnailSearchQuery).forEach(thumbnailElement => applyThumbnail(thumbnailElement));
+}
+
+kuru_image.src = SourceUrl;
+kuru_image.onload = () => {
+    setInterval(applyThumbnailToImages, 100);
+}
